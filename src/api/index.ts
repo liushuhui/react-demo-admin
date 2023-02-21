564a2c7c4dd4ad369b630e4465e96a88c3c08cce
liushuhui <394507099@qq.com>
2023-02-21 09:30:36 +0800

    分页，排序

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,10 @@
-import ajax from './ajax';
-const baseUrl = 'http://localhost:3000';
+import ajax from "./ajax";
+const baseUrl = "http://localhost:3000";
 export const addData = (data: any): Promise<any> =>
-  ajax(`${baseUrl}/test/add`, data, 'post');
+  ajax(`${baseUrl}/test/add`, data, "post");
+
 export const getAll = (data: any) =>
-  ajax(`${baseUrl}/test/getAll`, data, 'GET');
+  ajax(`${baseUrl}/test/getAll`, data, "post");
+
+export const tableExport = (data?: any) =>
+  ajax(`${baseUrl}/test/tableExport`, data, "post");
--- src/pages/formSubmit.tsx
+++ src/pages/formSubmit.tsx
@@ -1,19 +1,26 @@
 import { Button, Form, Input, InputNumber, Table, message } from 'antd';
-import { addData, getAll } from '../api';
+import { addData, getAll, tableExport } from '../api';
 import { useEffect, useState } from 'react';
 import { ProTable } from '@ant-design/pro-components';
 
 const FormSubmit = () => {
   const [form] = Form.useForm();
-  const [tableData, setTableData] = useState<any>([]);
   const onFinish = async (values: any): Promise<any> => {
     const { code } = await addData(values);
     code === 200 ? getData() : message.error('error');
   };
-  const getData = async () => {
-    const result: any = await getAll({ pageSize: 5, current: 1 });
-    console.log(result);
-    setTableData(result?.data);
+  const getData = async (params = {}, sorter?: any) => {
+    console.log('params', params, sorter);
+    const sortParams = {
+      sorter
+    }
+    const req = { ...params, ...sortParams }
+    const result: any = await getAll(req);
+    return {
+      data: result.data,
+      total: result.count,
+      success: true,
+    };
   };
   const colums: any[] = [
     {
@@ -26,7 +33,9 @@ const FormSubmit = () => {
       title: '年龄',
       dataIndex: 'age',
       key: 'age',
-      align: 'center'
+      align: 'center',
+      defaultSortOrder: 'ascend',
+      sorter: true
     },
     {
       title: '爱好',
@@ -35,16 +44,40 @@ const FormSubmit = () => {
       align: 'center'
     }
   ];
+  const exportTable = async () => {
+    const res = await tableExport()
+    const content: any = res;
+    const elink = document.createElement('a');
+    elink.download = 'dataRecordlist.xls'; 
+    elink.style.display = 'none';
+    const blob = new Blob([content]);
+    elink.href = URL.createObjectURL(blob);
+    document.body.appendChild(elink);
+    elink.click();
+    document.body.removeChild(elink);
+  }
   useEffect(() => {
-    getData();
+    getData({ pageSize: 5, current: 1 });
   }, []);
   return (
-    <div style={{ display: 'flex' }}>
+    <div style={{ display: 'flex', marginTop: '20px' }}>
       <ProTable
+        style={{ width: '80vw', marginRight: '40px' }}
         rowKey="id"
-        dataSource={tableData}
+        request={getData}
         columns={colums}
-        style={{ width: '50vw' }}
+        options={false}
+        bordered
+        pagination={{
+          defaultPageSize: 5,
+          defaultCurrent: 1,
+          showQuickJumper: true,
+          showSizeChanger: true
+        }}
+        toolBarRender={() => [
+          <Button type='primary' key="export" onClick={() => exportTable()}>导出</Button>,
+          <Button type='primary' danger key="import">导入</Button>,
+        ]}
       />
       <Form form={form} onFinish={onFinish}>
         <Form.Item label="姓名" name="name">
